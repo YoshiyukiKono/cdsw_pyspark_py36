63c548f999d90d61b805abcab45738ad7cb634fd
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2019-12-04 12:52:37 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-conda create -n py36 -y python=3.6 numpy pip sklearn joblibspark
+conda create -n py36 -y python=3.6 numpy pip scikit-learn
 source activate py36
 cp -r ~/.local/lib ~/.conda/envs/py36/
 source deactivate
